1da4a7f2154e9d20cedfd929924461b2f4d110f2
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-19 12:42:29 +0200

    [add] - DSC basics

--- src/DecentralisedStableCoin.sol
+++ src/DecentralisedStableCoin.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract DecentralisedStableCoin {}
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DecentralisedStableCoin is ERC20Burnable, Ownable {
+    /* Constructor */
+    constructor() ERC20("Decentralised Stable Coin", "DSC") Ownable(msg.sender) {}
+
+    /* Functions */
+    function burn(uint256 _value) public override onlyOwner {}
+
+    function mint(address _to, _uint256 amount) external onlyOwner returns (bool) {}
+}
