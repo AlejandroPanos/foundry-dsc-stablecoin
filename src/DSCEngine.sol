94b30cda55e3b1e8e8d365d0a51a761727e9b13d
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-19 13:40:30 +0200

    [add] - Constant variables

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -40,6 +40,13 @@ contract DSCEngine is ReentrancyGuard {
     /* ============================================================ */
     /* State variables                                              */
     /* ============================================================ */
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant PRECISION = 1e18;
+    uint256 private constant LIQUIDATION_THRESHOLD = 50;
+    uint256 private constant LIQUIDATION_PRECISION = 100;
+    uint256 private constant MIN_HEALTH_FACTOR = 1e18;
+    uint256 private constant LIQUIDATION_BONUS = 10;
+
     /**
      * @dev Mapping to keep track of a token's price feed address.
      */
