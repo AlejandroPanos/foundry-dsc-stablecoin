8ae7380b56d24e09398f5240393fa31e31a83dc4
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-19 12:45:48 +0200

    [add] - Burning function

--- src/DecentralisedStableCoin.sol
+++ src/DecentralisedStableCoin.sol
@@ -6,11 +6,27 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DecentralisedStableCoin is ERC20Burnable, Ownable {
+    /* Errors */
+    error DecentralisedStableCoin__MustBeGreatedThanZero();
+    error DecentralisedStableCoin__BalanceMustBeGreatedThanValue();
+
     /* Constructor */
     constructor() ERC20("Decentralised Stable Coin", "DSC") Ownable(msg.sender) {}
 
     /* Functions */
-    function burn(uint256 _value) public override onlyOwner {}
+    function burn(uint256 _value) public override onlyOwner {
+        uint256 userBalance = balanceOf(msg.sender);
+
+        if (_value <= 0) {
+            revert DecentralisedStableCoin__MustBeGreatedThanZero();
+        }
+
+        if (userBalance < _value) {
+            revert DecentralisedStableCoin__BalanceMustBeGreatedThanValue();
+        }
+
+        super.burn(_value);
+    }
 
     function mint(address _to, _uint256 amount) external onlyOwner returns (bool) {}
 }
