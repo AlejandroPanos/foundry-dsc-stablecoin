78bcb356c359930600517f8bd19bbf7ebbebc973
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-19 13:04:47 +0200

    [add] - Engine bases

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,4 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract DSCEngine {}
+import {DecentralisedStableCoin} from "src/DecentralisedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {OracleLib} from "src/libraries/OracleLib.sol";
+
+contract DSCEngine is ReentrancyGuard {
+    /* Libraries */
+    using OracleLib for AggregatorV3Interface;
+
+    /* Errors */
+
+    /* State variables */
+
+    /* Constructor */
+    constructor() {}
+
+    /* Functions */
+}
