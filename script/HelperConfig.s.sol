551df3f06cd5fe2fcf6cf2884dd65d63091b4c08
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-20 17:56:01 +0200

    [add] - Config bases

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,4 +5,26 @@ import {MockV3Aggregator} from "test/mocks/MockV3Aggregator.sol";
 import {Script} from "forge-std/Script.sol";
 import {ERC20Mock} from "test/mocks/ERC20Mock.sol";
 
-contract HelperConfig is Script {}
+contract HelperConfig is Script {
+    /* Instantiate a config */
+    NetworkConfig public activeNetworkConfig;
+
+    /* Type variables */
+    struct NetworkConfig {
+        address wethUsdPriceFeed;
+        address wbtcUsdPriceFeed;
+        address weth;
+        address wbtc;
+        uint256 deployerKey;
+    }
+
+    /* State variables */
+    uint8 private constant DECIMALS = 8;
+    int256 private constant ETH_USD_PRICE = 2000e8;
+    int256 private constant BTC_USD_PRICE = 1000e8;
+
+    address private constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
+    /* Constructor */
+    constructor() {}
+}
