1c41a665af614935b2641698ea21a3794aaa13c3
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-20 18:23:41 +0200

    [add] - Deployment bases

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -5,4 +5,13 @@ import {Script} from "forge-std/Script.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 import {DSCEngine} from "src/DSCEngine.sol";
 
-contract DeployDSC is Script {}
+contract DeployDSC is Script {
+    /* Instantiate contracts */
+    HelperConfig private helperConfig;
+    DSCEngine private engine;
+
+    /* State variables */
+
+    /* Run function */
+    function run() external returns (DSCEngine, HelperConfig) {}
+}
