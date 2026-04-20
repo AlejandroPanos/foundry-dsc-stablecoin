e89a62068518d758b90d7b7b766f7af9be912c27
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-20 18:29:08 +0200

    [add] - Run function

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,14 +4,43 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 import {DSCEngine} from "src/DSCEngine.sol";
+import {DecentralisedStableCoin} from "src/DecentralisedStableCoin.sol";
 
 contract DeployDSC is Script {
     /* Instantiate contracts */
     HelperConfig private helperConfig;
     DSCEngine private engine;
+    DecentralisedStableCoin private dsc;
 
     /* State variables */
+    address wethUsdPriceFeed;
+    address wbtcUsdPriceFeed;
+    address weth;
+    address wbtc;
+    uint256 deployerKey;
+
+    address[] tokenAddresses;
+    address[] priceFeedAddresses;
 
     /* Run function */
-    function run() external returns (DSCEngine, HelperConfig) {}
+    function run() external returns (DecentralisedStableCoin, DSCEngine, HelperConfig) {
+        helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getActiveNetworkConfig();
+        wethUsdPriceFeed = config.wethUsdPriceFeed;
+        wbtcUsdPriceFeed = config.wbtcUsdPriceFeed;
+        weth = config.weth;
+        wbtc = config.wbtc;
+        deployerKey = config.deployerKey;
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast();
+        dsc = new DecentralisedStableCoin();
+        engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(engine));
+        vm.stopBroadcast();
+
+        return (dsc, engine, helperConfig);
+    }
 }
