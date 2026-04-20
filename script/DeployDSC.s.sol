89e52bcef5b25dcfa9b5dfeeda631e63e9ecece3
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-20 18:14:24 +0200

    [add] - Deploy script file

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+
+contract DeployDSC is Script {}
