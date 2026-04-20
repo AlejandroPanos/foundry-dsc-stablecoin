54b8acb7d0f4e1e421fe1c837b0a60a8df59f992
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-20 18:02:33 +0200

    [add] - Constructor & sepolia config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -25,6 +25,29 @@ contract HelperConfig is Script {
 
     address private constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
+    uint256 public constant SEPOLIA_ID = 11155111;
+
     /* Constructor */
-    constructor() {}
+    constructor() {
+        if (block.chainid = SEPOLIA_ID) {
+            activeNetworkConfig = getSepoliaConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilConfig();
+        }
+    }
+
+    /* Functions */
+    function getSepoliaConfig() public returns (NetworkConfig memory) {
+        NNetworkConfig memory sepoliaConfig = NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+
+        return sepoliaConfig;
+    }
+
+    function getOrCreateAnvilConfig() public {}
 }
