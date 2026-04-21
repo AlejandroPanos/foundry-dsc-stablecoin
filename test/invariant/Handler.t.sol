03c33844a2d1789a6be0a71dfdc2ba078fe3266c
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-21 18:29:13 +0200

    [add] - Imports for handler

--- test/invariant/Handler.t.sol
+++ test/invariant/Handler.t.sol
@@ -2,5 +2,16 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "script/DeployDSC.s.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {DecentralisedStableCoin} from "src/DecentralisedStableCoin.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {ERC20Mock} from "test/mocks/ERC20Mock.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MockV3Aggregator} from "@chainlink/contracts/src/v0.8/shared/mocks/MockV3Aggregator.sol";
 
-contract Handler is Test {}
+contract Handler is Test {
+    /* Instantiate contracts */
+
+    }
