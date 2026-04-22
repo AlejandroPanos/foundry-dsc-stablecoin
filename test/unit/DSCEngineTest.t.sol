96fe1e5d3e5883c5c7852ed19571deec28503637
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-22 16:53:13 +0200

    [add] - Variables

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -8,4 +8,22 @@ import {DecentralisedStableCoin} from "src/DecentralisedStableCoin.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 import {ERC20Mock} from "test/mocks/ERC20Mock.sol";
 
-contract DSCEngineTest is Test {}
+contract DSCEngineTest is Test {
+    /* Instantiate contracts */
+    DeployDSC deployer;
+    DSCEngine engine;
+    DecentralisedStableCoin dsc;
+    HelperConfig helperConfig;
+
+    /* State variables */
+    address ethUsdPriceFeed;
+    address btcUsdPriceFeed;
+    address weth;
+
+    address public USER = makeAddr("USER");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+}
