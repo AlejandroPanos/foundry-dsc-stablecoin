caa1903c05b9dea5dbbed1d5f45f616e4d584206
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-22 16:49:53 +0200

    [add] - Unit testing file

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "script/DeployDSC.s.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {DecentralisedStableCoin} from "src/DecentralisedStableCoin.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {ERC20Mock} from "test/mocks/ERC20Mock.sol";
+
+contract DSCEngineTest is Test {}
