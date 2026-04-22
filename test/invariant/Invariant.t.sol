f66c989a7ebad20165ace0962b29abb6270e6a0e
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-22 15:55:37 +0200

    [add] - Invariant file

---
+++ test/invariant/Invariant.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "script/DeployDSC.s.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {DecentralisedStableCoin} from "src/DecentralisedStableCoin.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {ERC20Mock} from "test/mocks/ERC20Mock.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MockV3Aggregator} from "@chainlink/contracts/src/v0.8/shared/mocks/MockV3Aggregator.sol";
+
+contract Invariant is StdInvariant, Test {}
